333ab9beab4fd24ac74e30ea0ad406ad38c50db1
Leonan Marques <80500276+leonanmar886@users.noreply.github.com>
2021-09-16 15:31:24 -0300

    Add files via upload

---
+++ src/main/java/digital/innovation/one/personmanagerapi/PersonmanagerapiApplication.java
@@ -0,0 +1,13 @@
+package digital.innovation.one.personmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonmanagerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonmanagerapiApplication.class, args);
+	}
+
+}
