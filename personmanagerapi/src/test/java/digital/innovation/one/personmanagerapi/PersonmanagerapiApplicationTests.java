a1e535ae036d9a3bbea63c4adced3850a03d491d
leonanmar886 <leonan.marques886@gmail.com>
2021-09-16 14:38:37 -0300

    First commit

---
+++ personmanagerapi/src/test/java/digital/innovation/one/personmanagerapi/PersonmanagerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.innovation.one.personmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonmanagerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
