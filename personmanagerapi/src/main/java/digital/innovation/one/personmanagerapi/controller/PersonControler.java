a5b3294fa0ef094f1e64f1025f23d76087fe90c4
leonanmar886 <leonan.marques886@gmail.com>
2021-09-21 11:48:15 -0300

    Created the method to show all the registered people

--- personmanagerapi/src/main/java/digital/innovation/one/personmanagerapi/controller/PersonControler.java
+++ personmanagerapi/src/main/java/digital/innovation/one/personmanagerapi/controller/PersonControler.java
@@ -2,13 +2,13 @@ package digital.innovation.one.personmanagerapi.controller;
 
 import digital.innovation.one.personmanagerapi.dto.request.PersonDTO;
 import digital.innovation.one.personmanagerapi.dto.response.MessageResponseDTO;
-import digital.innovation.one.personmanagerapi.entity.Person;
 import digital.innovation.one.personmanagerapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -27,5 +27,8 @@ public class PersonControler {
         return personService.createPerson(personDTO);
     }
 
-
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
 }
